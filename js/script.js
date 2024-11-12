73004c4d75f6743ab0b35b907f5dbd6adc2249e5
vikas-11-dixena <vikasdixena79@gmail.com>
2024-11-12 12:37:13 +0530

    random color generator use for hexdecimal color code

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const getColor = () => {
+    // Hex code
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = "#" + randomNumber.toString(16);
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerText = randomCode;
+}
+// event call
+document.getElementById("btn").addEventListener(
+    "click",getColor);
+
+    // initial call
+getColor();
